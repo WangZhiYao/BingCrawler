ae146dbd652938acabb14b0269ab2367a8f664a8
WangZhiYao <393893729@qq.com>
2022-03-16 23:50:19 +0800

    Update Crawler to use Bing Api

--- BingCrawler.py
+++ BingCrawler.py
@@ -2,8 +2,9 @@ import json
 import logging
 from datetime import datetime, timezone, timedelta
 from pathlib import Path
+from types import SimpleNamespace
 
-from sqlalchemy import Column, String, Integer, create_engine, desc
+from sqlalchemy import Column, String, Integer, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -12,9 +13,7 @@ from tenacity import retry, retry_if_exception_type, stop_after_attempt, wait_fi
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
 
-BING_URL = 'https://cn.bing.com/'
-BING_IMAGE_URL = BING_URL + 'HPImageArchive.aspx'
-BING_PARAMS = {'format': 'js', 'idx': '0', 'n': '1', 'mkt': 'zh-CN'}
+BING_API_URL = 'https://cn.bing.com/hp/api/model'
 
 TZ_SHANGHAI = timezone(timedelta(hours=8), name='Asia/Shanghai')
 TODAY = datetime.utcnow().replace(tzinfo=timezone.utc).astimezone(TZ_SHANGHAI)
@@ -63,28 +62,25 @@ class CrawlerException(Exception):
 
 @retry(retry=retry_if_exception_type(CrawlerException), stop=stop_after_attempt(3), wait=wait_fixed(5), )
 def get_hp_image_archive():
-    response = requests.get(BING_IMAGE_URL, params=BING_PARAMS)
+    response = requests.get(BING_API_URL)
     response.raise_for_status()
-    hp_image_archive = json.loads(response.text)
-    is_image_updated(hp_image_archive)
+    hp_image_archive = json.loads(response.text, object_hook=lambda d: SimpleNamespace(**d))
+    check_image_updated(hp_image_archive)
     download_image(hp_image_archive)
     insert_to_db(hp_image_archive)
 
 
-def is_image_updated(hp_image_archive):
-    if hp_image_archive['images']:
-        image = hp_image_archive['images'][0]
-        image_date = image['enddate']
-        if image_date != TODAY.strftime('%Y%m%d'):
-            raise CrawlerException('date not updated.')
+def check_image_updated(hp_image_archive):
+    if hp_image_archive.MediaContents[0].ImageContent.TriviaId.split('_')[1] != TODAY.strftime('%Y%m%d'):
+        raise CrawlerException('date not updated.')
 
 
 def download_image(hp_image_archive):
     try:
-        image = hp_image_archive['images'][0]
-        image_url = image['url'][1:]
-        image_file_name = image_url.split('&')[0].replace('th?id=OHR.', '')
-        response = requests.get(BING_URL + image_url, stream=True)
+        image = hp_image_archive.MediaContents[0].ImageContent.Image
+        image_url = image.Url.replace('1920x1080', 'UHD')
+        image_file_name = image_url.split('&')[0].split('=')[1].replace('OHR.', '').replace('1920x1080', 'UHD')
+        response = requests.get(image_url, stream=True)
         response.raise_for_status()
 
         IMAGE_DIR.mkdir(parents=True, exist_ok=True)
@@ -98,19 +94,20 @@ def download_image(hp_image_archive):
 
 def insert_to_db(hp_image_archive):
     try:
-        image = hp_image_archive['images'][0]
-        image_url = image['url'][1:]
-        image_file_name = image_url.split('&')[0].replace('th?id=OHR.', '')
-        date = datetime.strptime(image['enddate'], '%Y%m%d')
+        image_content = hp_image_archive.MediaContents[0].ImageContent
+        image = image_content.Image
+        image_url = image.Url.replace('1920x1080', 'UHD')
+        image_file_name = image_url.split('&')[0].split('=')[1].replace('OHR.', '').replace('1920x1080', 'UHD')
+        date = datetime.strptime(image_content.TriviaId.split('_')[1], '%Y%m%d')
         conv = lambda s: s or None
         bing_image = BingImage(
             date.year,
             date.month,
             date.day,
-            conv(image.get('copyright', None)),
-            conv(image.get('title', None)),
-            conv(image.get('description', None)),
-            conv(image.get('location', None)),
+            conv(image_content.Copyright),
+            conv(image_content.Title),
+            conv(image_content.Description),
+            conv(None),
             image_file_name
         )
         session.add(bing_image)
